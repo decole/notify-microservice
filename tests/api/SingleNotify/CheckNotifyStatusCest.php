9e03b1e379e0b4460e017b21b5039194e7e64b12
decole <decole2014@yandex.ru>
2022-07-12 20:42:54 +0300

    finish api test

--- tests/api/SingleNotify/CheckNotifyStatusCest.php
+++ tests/api/SingleNotify/CheckNotifyStatusCest.php
@@ -8,10 +8,11 @@ use App\Application\Http\Api\SingleNotify\Dto\MessageDto;
 use App\Domain\Doctrine\NotifyMessage\Entity\NotifyMessage;
 use App\Infrastructure\Doctrine\Service\NotifyMessageService;
 use App\Tests\ApiTester;
+use Ramsey\Uuid\Uuid;
 
 class CheckNotifyStatusCest
 {
-    public function checkStatusNotify(ApiTester $I): void
+    public function checkStatusNotifyInQueue(ApiTester $I): void
     {
         $entity = $this->createEntity($I);
 
@@ -28,6 +29,82 @@ class CheckNotifyStatusCest
         ]);
     }
 
+    public function checkStatusActive(ApiTester $I): void
+    {
+        $entity = $this->createEntity($I);
+
+        $entity->setStatus(NotifyMessage::STATUS_ACTIVE);
+
+        $this->updateEntity($entity, $I);
+
+        $id = $entity->getId()->toString();
+
+        $I->sendGet("/v1/check-status/{$id}");
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            'id' => $id,
+            'status' => NotifyMessage::TEXT_STATUS_MAP[NotifyMessage::STATUS_ACTIVE],
+            'lastModifiedStatusByUTC' => $entity->getUpdatedAt()->format('Y-m-d H:i:s'),
+        ]);
+    }
+
+    public function checkStatusDone(ApiTester $I): void
+    {
+        $entity = $this->createEntity($I);
+
+        $entity->setStatus(NotifyMessage::STATUS_DONE);
+
+        $this->updateEntity($entity, $I);
+
+        $id = $entity->getId()->toString();
+
+        $I->sendGet("/v1/check-status/{$id}");
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            'id' => $id,
+            'status' => NotifyMessage::TEXT_STATUS_MAP[NotifyMessage::STATUS_DONE],
+            'lastModifiedStatusByUTC' => $entity->getUpdatedAt()->format('Y-m-d H:i:s'),
+        ]);
+    }
+
+    public function checkStatusError(ApiTester $I): void
+    {
+        $entity = $this->createEntity($I);
+
+        $entity->setStatus(NotifyMessage::STATUS_ERROR);
+
+        $this->updateEntity($entity, $I);
+
+        $id = $entity->getId()->toString();
+
+        $I->sendGet("/v1/check-status/{$id}");
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            'id' => $id,
+            'status' => NotifyMessage::TEXT_STATUS_MAP[NotifyMessage::STATUS_ERROR],
+            'lastModifiedStatusByUTC' => $entity->getUpdatedAt()->format('Y-m-d H:i:s'),
+        ]);
+    }
+
+    public function notFoundNotifyMessage(ApiTester $I): void
+    {
+        $id = Uuid::uuid4();
+
+        $I->sendGet("/v1/check-status/{$id}");
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->seeResponseIsJson();
+        $I->canSeeResponseCodeIs(400);
+        $I->seeResponseContainsJson([
+            'error' => "Notify by id {$id} not found",
+        ]);
+    }
+
     private function createEntity(ApiTester $I): NotifyMessage
     {
         $body = [
@@ -38,9 +115,20 @@ class CheckNotifyStatusCest
 
         $dto = new MessageDto(NotifyMessage::EMAIL_TYPE, $body);
 
-        /** @var NotifyMessageService $service */
-        $service = $I->grabService(NotifyMessageService::class);
+        $service = $this->getService($I);
 
         return $service->create($dto);
+    }
+
+    private function updateEntity(NotifyMessage $entity, ApiTester $I): void
+    {
+        $service = $this->getService($I);
+
+        $service->update($entity);
+    }
+
+    private function getService(ApiTester $I): NotifyMessageService
+    {
+        return $I->grabService(NotifyMessageService::class);
     }
 }
---
+++ tests/api/SingleNotify/SingleSendEmailCest.php
@@ -0,0 +1,129 @@
+<?php
+
+
+namespace App\Tests\api\SingleNotify;
+
+
+use App\Domain\Doctrine\NotifyMessage\Entity\NotifyMessage;
+use App\Tests\ApiTester;
+
+class SingleSendEmailCest
+{
+    public function positiveSend(ApiTester $I): void
+    {
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendPost('/v1/send', [
+            'type' => NotifyMessage::EMAIL_TYPE,
+            'email' => 'decole@rambler.ru',
+            'message' => 'tester',
+        ]);
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            'status' => 'in queue',
+        ]);
+        $I->seeInRepository(NotifyMessage::class, [
+            'type' => 'email',
+            'status' => NotifyMessage::STATUS_IN_QUEUE,
+        ]);
+    }
+
+    public function negativeWrongType(ApiTester $I): void
+    {
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendPost('/v1/send', [
+            'type' => 'error',
+            'message' => 'test',
+        ]);
+        $I->seeResponseIsJson();
+        $I->canSeeResponseCodeIs(400);
+        $I->seeResponseContainsJson([
+            'error' => 'Validation criteria by notify type not found.',
+        ]);
+    }
+
+    public function negativeEmptyMessage(ApiTester $I): void
+    {
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendPost('/v1/send', [
+            'type' => NotifyMessage::EMAIL_TYPE,
+            'email' => 'decole@rambler.ru',
+            'message' => '',
+        ]);
+        $I->seeResponseIsJson();
+        $I->canSeeResponseCodeIs(400);
+        $I->seeResponseContainsJson([
+            'error' => [
+                'message' => 'This value should not be null.',
+            ],
+        ]);
+    }
+
+    public function negativeNullMessage(ApiTester $I): void
+    {
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendPost('/v1/send', [
+            'type' => NotifyMessage::EMAIL_TYPE,
+            'email' => 'decole@rambler.ru',
+            'message' => null,
+        ]);
+        $I->seeResponseIsJson();
+        $I->canSeeResponseCodeIs(400);
+        $I->seeResponseContainsJson([
+            'error' => [
+                'message' => 'This value should not be null.',
+            ],
+        ]);
+    }
+
+    public function negativeNullEmail(ApiTester $I): void
+    {
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendPost('/v1/send', [
+            'type' => NotifyMessage::EMAIL_TYPE,
+            'email' => null,
+            'message' => 'test',
+        ]);
+        $I->seeResponseIsJson();
+        $I->canSeeResponseCodeIs(400);
+        $I->seeResponseContainsJson([
+            'error' => [
+                'email' => 'field is required!',
+            ],
+        ]);
+    }
+
+    public function negativeEmptyEmail(ApiTester $I): void
+    {
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendPost('/v1/send', [
+            'type' => NotifyMessage::EMAIL_TYPE,
+            'email' => '',
+            'message' => 'test',
+        ]);
+        $I->seeResponseIsJson();
+        $I->canSeeResponseCodeIs(400);
+        $I->seeResponseContainsJson([
+            'error' => [
+                'email' => 'field is required!',
+            ],
+        ]);
+    }
+
+    public function negativeWrongEmail(ApiTester $I): void
+    {
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendPost('/v1/send', [
+            'type' => NotifyMessage::EMAIL_TYPE,
+            'email' => 'test@mimimi',
+            'message' => 'test',
+        ]);
+        $I->seeResponseIsJson();
+        $I->canSeeResponseCodeIs(400);
+        $I->seeResponseContainsJson([
+            'error' => [
+                'email' => 'This value is not a valid email address.',
+            ],
+        ]);
+    }
+}
